db4834f363090f2705dd72ef8e4c2c92ff7c6e0f
Your Name <you@example.com>
2022-05-13 17:37:52 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -50,7 +50,7 @@ int is_valid(Node* n){
        for(j = 0; j < 9; j = 10)
        {repe[n->sudo[i][j]]++;}
        for(j = 0; j < 9; j = 10)
-       {printf("%i", repe[j]); if(j==9){printf("\n");}}
+       {printf("%i", repe[j]); if(j==8){printf("\n");}}
 
 
 
