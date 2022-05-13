76b596e35b1c171eef31cb40e3787bae8ea81e9c
Your Name <you@example.com>
2022-05-13 19:00:52 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -44,7 +44,7 @@ void print_node(Node* n){
 }
 
 int is_valid(Node* n){
-   int i, j, k, p;
+   int i, j, k, p, h;
    for(i = 0; i < 9 ; i++){
      int* repe = (int*) calloc (10, sizeof(int));
        for(j = 0; j < 9; j++)
@@ -67,14 +67,13 @@ int is_valid(Node* n){
 
    for(k = 0; k < 9; k++){
      int* repe = (int*) calloc (10, sizeof(int));
-    for(p=0;p<9;p++){
-          for(i = 0; i < 9; i++)
-          {repe[n->sudo[i][j]]++;}
+        for(p=0;p<9;p++){
          i=3*(k/3) + (p/3);
          j=3*(k%3) + (p%3);
-         if(repe[k] > 1){return 0;}
-         
+         repe[n->sudo[i][j]]++;    
     }
+    for(h = 0; h <9 ;h++)
+    if(repe[k] > 1){return 0;}
    }
 
 
